4905e2473ae3bc30133b6567b11665a9d5288f6e
Kan-jas <kajio0024@gmail.com>
2020-12-31 00:37:28 +0900

    first enviroment.

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,3 @@
-// This file will add both p5 instanced and global intellisence
 import module = require('p5');
 import * as p5Global from 'p5/global'
 
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,31 @@
 import * as p5 from 'p5';
 
+
+// Global variables
+const float radius = 50.0;
+const int X, Y;
+const int nX, nY;
+const int delay = 16;
+
 const sketch = (p: p5) => {
-    p.preload = () => {
-    };
 
-    p.setup = () => {
-        p.resizeCanvas(p.windowWidth, p.windowHeight);
-    };
 
-    p.windowResized = () => {
-        p.resizeCanvas(p.windowWidth, p.windowHeight);
-    };
 
-    p.draw = () => {
-        p.background(100);
-        p.ellipse(50, 50, 80, 80);
-    }
+    // p.preload = () => {
+    // };
+
+    // p.setup = () => {
+    //     p.resizeCanvas(p.windowWidth, p.windowHeight);
+    // };
+
+    // p.windowResized = () => {
+    //     p.resizeCanvas(p.windowWidth, p.windowHeight);
+    // };
+
+    // p.draw = () => {
+    //     p.background(100);
+    //     p.ellipse(50, 50, 80, 80);
+    // }
 };
 
 const sketchP = new p5(sketch);
