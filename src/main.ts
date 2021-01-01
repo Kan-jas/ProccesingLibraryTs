b5eaf6e9b86b3e82195b70682f3f87670ee44b80
Kan-jas <kajio0024@gmail.com>
2021-01-01 23:51:54 +0900

    チュートリアルあってみた

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,27 @@ import * as p5 from 'p5';
 
 
 // Global variables
-const float radius = 50.0;
-const int X, Y;
-const int nX, nY;
-const int delay = 16;
 
 const sketch = (p: p5) => {
 
 
 
-    // p.preload = () => {
-    // };
+    p.preload = () => {
+    };
 
-    // p.setup = () => {
-    //     p.resizeCanvas(p.windowWidth, p.windowHeight);
-    // };
+    p.setup = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+    };
 
-    // p.windowResized = () => {
-    //     p.resizeCanvas(p.windowWidth, p.windowHeight);
-    // };
+    p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+    };
 
-    // p.draw = () => {
-    //     p.background(100);
-    //     p.ellipse(50, 50, 80, 80);
-    // }
+    p.draw = () => {
+        p.background(50, 200, 155);
+        p.ellipse(50, 50, 80, 80);
+        p.ellipse(100, 100, 100, 100);
+    }
 };
 
 const sketchP = new p5(sketch);
